38e40b909904712bfb9d5cda70513351e33351d2
adetion <36150203+adetion@users.noreply.github.com>
2022-10-12 03:54:30 +0800

    Update txt2utf8.py

--- txt2utf8.py
+++ txt2utf8.py
@@ -4,6 +4,7 @@ file_ext = '.txt|.csv'
 yourpath = '../txt/03古典相关'
 yourfile = '../txt/03古典相关/红楼梦/红楼梦.txt'
 
+# 函数声明开始
 def allpath_txt(yourpath, file_ext):
     txtfile.allpath_txt_encoding_to_utf8(yourpath, file_ext)
 
@@ -12,7 +13,7 @@ def path_txt(yourpath, file_ext):
 
 def file_txt(yourfile, file_ext):
     txtfile.file_txt_encoding_to_utf8(yourfile, file_ext)
-
+# 函数声明结束
 
 # 程序入口
 if __name__ == '__main__':
